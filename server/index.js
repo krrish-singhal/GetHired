40b417564889c4e6451469cee1b9aeb93fa960b3
Mukul <mukulkashyap.business@gmail.com>
2026-04-29 19:00:42 +0530

    update cors for production

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://gethired-psi.vercel.app"
+  origin: "https://gethiredapp.vercel.app"
 }));
 app.use(express.json());
 
